8bf335b5b87a50c722bb306e924c68e86565e06c
J-Roux <aps7@tut.by>
2016-10-25 00:24:23 +0300

    change logic

--- lab2/main.py
+++ lab2/main.py
@@ -21,6 +21,34 @@ def get_param(val):
     return alpha_list[randint(0, len(alpha_list) - 1)]
 
 
+def code_text(text, a, b, p):
+    alpha = get_param(a)
+    beta = get_param(b)
+    return  [ (char**a % p) ** b % p  for char in text]
+    
+def decode_text(encode_text, p):
+
+    alpha_t = [ encode_text[0] ** i % p for i in range(0, p)]
+    print(alpha_t)    
+    a_t = [get_multiplier(alpha, phi(p), 1) for alpha in alpha_t]
+    a_t = []
+    for alpha in alpha_t:
+        a = get_multiplier(alpha, phi(p), 1)
+        if a :
+            
+    #beta_t = [ encode_text[0] ** a % p for a in a_t]
+    #b_t = [get_multiplier(beta, phi(p), 1) for beta in beta_t]
+    print(alpha_t)
+    print(a_t)
+   # print(beta_t)
+   # print(b_t)
+
+
+encode_text = code_text([message], a, b, p)
+print(encode_text)
+decode_text(encode_text, p)
+
+
 alpha = get_param(a)
 beta = get_param(b)
 print ('alpha = %i' % alpha)
@@ -45,7 +73,7 @@ alpha_t = [i for i in range(0, p) if u2 ** i % p == u3][0]
 print('alpha = %i' %  alpha_t)
 a_t = get_multiplier(alpha_t, phi(p),1)[0]
 print('a = %i' % a_t)
-beta_t = u2 ** a % p 
+beta_t = u2 ** a_t % p 
 print('beta = %i' % beta_t)
 b_t = get_multiplier(beta_t, phi(p),1)[0]
 print('b = %i' % b_t)
