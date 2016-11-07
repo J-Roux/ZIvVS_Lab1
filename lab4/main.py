b1962e094aec51a21f14438b2a564200dd63dbc5
J-Roux <aps7@tut.by>
2016-11-07 23:23:05 +0300

    lab4

--- lab4/main.py
+++ lab4/main.py
@@ -11,6 +11,7 @@ test_arr = [
     [1, 2]
 ]
 
+s = 5
 
 size = int(input('Enter rucksack size  '))
 N = int(input('Enter number of generations  '))
@@ -26,12 +27,27 @@ while result != "":
         break
     items.append([int(i) for i in item.split(' ')])
 
-vec = [[ random.randint(0, 1) for i in range(0, len(items))] for i in range(0, 7)]
+vec = [[ random.randint(0, 1) for i in range(0, len(items))] for i in range(0, 13)]
 
 
 values = [ np.add.reduce( np.multiply(i , [ j[0] for j in items] )) for i in vec]
 weights = [ np.add.reduce( np.multiply(i , [ j[1] for j in items] )) for i in vec]
-
 data = zip(vec , zip(values, weights))
-parents = filter(lambda x:  x[1][1] <= size, data)
-print(list(parents))
+parents = list(filter(lambda x:  x[1][1] <= size, data))
+values = list(map(lambda x: x[1][0], parents))
+sigma = np.std(values)**0.5
+f_avg = np.mean(values)
+F = [ 1+( (f + f_avg)/2 * sigma)  for f in values]
+F_sum = sum(F)
+p =[ F_i / F_sum for F_i in F]
+res = set()
+while True:
+    i = np.random.choice(range(len(list(parents))), p=p)
+    res.add(parents[i])
+    if len(res) == s:
+        break
+
+print(res)
+    
+
+#print(np.array(list(parents)))
